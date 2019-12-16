a32ea5fa8125aa48c2b21ab73e9b70e2f31c8e18
jeffreytzeng <2013gi20@gmail.com>
2019-12-16 15:58:33 +0800

    Print Class Name With Api.
    Using __cxa_dmangle(typeid(this).name(), 0, 0, 0) in cxxabi.h to receive class name.

--- 30 Days of Code/Day 19 Interfaces/calculator.cpp
+++ 30 Days of Code/Day 19 Interfaces/calculator.cpp
@@ -1,10 +1,20 @@
+#include <cxxabi.h>
 #include <iostream>
+#include <typeinfo>
 
 using namespace std;
 
+// typeid(obj).name() returns type + number of characters + entry point in the c++ runtime library.
+// ex: typeid(this).name() returns P18AdvancedArithmatic (means pointer with 18 characters.)
 class AdvancedArithmatic {
 	public:
 		virtual int divisorSum(int n)= 0;
+		string BaseName() {
+			char *demangled_name = abi::__cxa_demangle(typeid(this).name(), 0, 0, 0);
+			string name(demangled_name);
+			free(demangled_name);
+			return name.substr(0, name.length()-1);
+		}
 };
 
 // Something wrong in HackerRank's compiler to use std::accumulate()
@@ -28,7 +38,8 @@ int main()
 	cin >> num;
 
 	AdvancedArithmatic *cal = new Calculator();
-	cout << "I implemented: AdvancedArithmatic" << endl;
+//	cout << "I implemented: AdvancedArithmatic" << endl;
+	cout << "I implemented: " << cal->BaseName() << endl;
 	cout << cal->divisorSum(num);
 	delete cal;
 }
